13e75e57fbfdd52b0b3e3e49f243a432566f70ee
fendull <john@laboe.com>
2022-07-15 22:05:25 -0700

    first commit

--- lib/cdk-static-site-stack.ts
+++ lib/cdk-static-site-stack.ts
@@ -1,8 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { 
+	Stack,
+	StackProps,
+	aws_s3 as s3,
+	aws_cloudfront as cf,
+	aws_cloudfront_origins as origins,
+	aws_s3_deployment as s3deploy
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStaticSiteStack extends Stack {
+export class StaticSiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -12,5 +19,26 @@ export class CdkStaticSiteStack extends Stack {
     // const queue = new sqs.Queue(this, 'CdkStaticSiteQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const accessIdentity = new cf.OriginAccessIdentity(scope, `StaticSiteAccessIdentity-${props.stackId}`);
+
+    const siteBucket = new s3.Bucket(scope, `StaticSiteBucket-${props.stackId}`, {
+	    bucketName: `StaticSiteBucket-${props.stackId}`,
+	    blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    siteBucket.grantRead(accessIdentity);
+
+    s3deploy.BucketDeployment(scope, `StaticSiteBucketDeployment-${props.stackId}`, {
+	    sources: [s3deploy.Source.asset(props.contentPath)],
+	    destinationBucket: siteBucket
+    });
+
+    const distro = new cf.Distribution(scope, `StaticSiteDistribution-${props.stackId}`, {
+	    defaultBehavior: {
+		    origin: new origins.S3Origin(siteBucket, {
+			    originAccessIdentity: accessIdentity 
+		    })
+	    },
+    });
   }
 }
